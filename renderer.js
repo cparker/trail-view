0cfeb4499a4d24aae619ce7f4e1b94ed44f914d4
Christian Parker <christian.parker@emc.com>
2017-05-24 09:41:15 -0600

    working well

--- renderer.js
+++ renderer.js
@@ -3,6 +3,7 @@
 // All of the Node.js APIs are available in this process.
 const xml2js = require('xml2js')
 const xml2jsParser = xml2js.Parser()
+const builder = new xml2js.Builder();
 const fs = require('fs')
 const moment = require('moment')
 const settings = require('electron-settings')
@@ -34,10 +35,15 @@ const playPauseElm = document.querySelector('#play-pause')
 const videoPositionElm = document.querySelector('#video-position')
 const trackPositionElm = document.querySelector('#track-position')
 const playbackSpeedElm = document.querySelector('#playback-speed')
-const showHeadingElm = document.querySelector('#show-heading')
 const headingElm = document.querySelector('#heading')
 const speedFieldElm = document.querySelector('#speed-field')
 const headingFieldElm = document.querySelector('#heading-field')
+const trackVideoLockElm = document.querySelector('#track-video-lock')
+const videoTypeElm = document.querySelector('#video-type')
+const compassElm = document.querySelector('#compass-field')
+const mapZoomElm = document.querySelector('#map-zoom')
+const newWaypointElm = document.querySelector('#new-waypoint')
+const addWaypointDialog = document.querySelector('.add-waypoint')
 
 const feetPerMeter = 3.28084
 
@@ -85,6 +91,17 @@ const usaTopoTiles =
     attribution: 'Copyright:© 2013 National Geographic Society, i-cubed'
   })
 
+const openCycleMapTiles =
+  L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=a5dd6a2f1c934394bce6b0fb077203eb', {
+    maxZoom: 22,
+    attribution: 'OpenCycleMap http://www.openstreetmap.org/'
+  })
+
+const openStreetMapTiles =
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 22,
+    attribution: 'OpenStreetMap http://www.openstreetmap.org/'
+  })
 
 
 const theMap = L.map('map', mapOptions).setView([initialMapLat, initialMapLon], 11)
@@ -105,11 +122,33 @@ const trackFileFilters = [{
 
 
 let trackStartMoment, videoStartMoment, trackLayer, tileLayer, checkVideoInterval, trackPoints, trackIndex,
-  trackMarker
+  trackMarker, locked, videoSliderPosition, trackSliderPosition, secondsPerVideoFrame,
+  lockedPositions, activeSlider, mapMouseEvent, gpxJSON, loadedGpxFile
 
 function initializeMap() {
   tileLayer = openTopoTiles
   tileLayer.addTo(theMap)
+  locked = false
+  trackSliderPosition = 0
+  videoSliderPosition = 0
+  trackIndex = 0
+  secondsPerVideoFrame = 1 / framesPerSecond
+  lockedPositions = {}
+  trackVideoLockElm.classList.remove('fa-lock')
+  trackVideoLockElm.classList.add('fa-unlock')
+  theMap.on('click', handleMapClick)
+  theMap.on('zoomend', z => {
+    console.log('zoom', z.target._zoom)
+    mapZoomElm.value = z.target._zoom
+  })
+}
+
+
+function handleMapClick(mapClickEvent) {
+  console.log('clicked map', mapClickEvent)
+  mapMouseEvent = mapClickEvent
+  addWaypointDialog.classList.remove('hidden')
+  newWaypointElm.focus()
 }
 
 function loadSettings() {
@@ -153,16 +192,69 @@ function loadSettings() {
     mapOverlayElm.style.opacity = parseInt(settings.get('mapOpacity')) / 100.0
     mapOpacityElm.value = settings.get('mapOpacity')
   }
+  if (settings.has('mapZoom')) {
+    mapZoomElm.value = settings.get('mapZoom')
+    // setting the actual map zoom is handled in loading the track
+  }
 
   if (settings.has('tileLayer')) {
     tileLayerElm.value = settings.get('tileLayer')
     handleChooseTileLayer()
   }
+
+  if (settings.has('secondsPerVideoFrame')) {
+    videoTypeElm.value = settings.get('secondsPerVideoFrame')
+    handleChooseVideoType()
+  }
+  if (settings.has('playbackSpeed')) {
+    videoElm.playbackRate = parseInt(settings.get('playbackSpeed')) / 100
+    playbackSpeedElm.value = settings.get('playbackSpeed')
+  }
+}
+
+
+function getTrackIndexForVideoPosition(allowBackwards) {
+
+  // based on the track locked positions, determine the
+  // {video: 1.166666, trackIndex: "10", time: "2017-05-06T14:26:40Z"}
+  const lockedTimeMoment = moment(lockedPositions.time)
+  // console.log('lockedTimeMoment', lockedTimeMoment.toISOString())
+  const seconds = (videoElm.currentTime - lockedPositions.video) * framesPerSecond * secondsPerVideoFrame
+  // console.log('seconds', seconds)
+  const realWorldVideoTimeAtCurrentPoint = lockedTimeMoment.clone().add(seconds, 'seconds')
+  // console.log('realWorldVideoTimeAtCurrentPoint', realWorldVideoTimeAtCurrentPoint.toISOString())
+
+  // now we have to find the track point closest to this time
+  // for each point (starting at trackIndex), is realWorldVideoTimeAtCurrentPoint >= point
+
+  // if allowBackwards is true, this means we need to start searching from the begining of the track points
+  // this is used when the user is scribbing with the range slider and it's possible they are scrubbing backwards
+  let i = allowBackwards ? 0 : trackIndex
+  for (let found = false; !found && i < trackPoints.length; i++) {
+    // console.log('i',i)
+
+    // console.log('diffing ', realWorldVideoTimeAtCurrentPoint.toISOString(), ' to ', trackPoints[i].moment.toISOString())
+    let dif = realWorldVideoTimeAtCurrentPoint.diff(trackPoints[i].moment)
+    // console.log('dif', dif)
+    if (dif < 0) {
+      found = true
+
+    }
+  }
+
+  return i - 1
 }
 
 
 function checkVideo() {
+  // update the position slider, which has one tick per frame
   videoPositionElm.value = videoElm.currentTime * framesPerSecond
+
+  if (locked) {
+    trackIndex = getTrackIndexForVideoPosition()
+    trackPositionElm.value = trackIndex
+    updateDisplayFromTrackPosition()
+  }
 }
 
 
@@ -194,10 +286,16 @@ function handleChooseTileLayer() {
     case 'usa-topo':
       tileLayer = usaTopoTiles
       break
+    case 'open-cycle':
+      tileLayer = openCycleMapTiles
+      break
+    case 'open-street':
+      tileLayer = openStreetMapTiles
+      break
   }
 
   tileLayer.addTo(theMap)
-  settings.set('tileLayer', this.value)
+  settings.set('tileLayer', this.value || tileLayerElm.value)
 }
 
 
@@ -362,7 +460,34 @@ function extractStartElevation(gpxData, meters) {
 }
 
 
-function drawGPXTrack(path) {
+function addStarMarker(text, lat, lon) {
+  try {
+    const starIcon = L.divIcon({
+      className: 'my-div-icon',
+      html: '<i class="fa fa-star fa-2x fa-red"></i>'
+    })
+
+    const textIcon = L.divIcon({
+      className: 'my-div-text-icon',
+      html: text
+    })
+
+    const textMarker = L.marker(L.latLng(lat, lon), {
+      icon: textIcon
+    })
+
+    const starMarker = L.marker(L.latLng(lat, lon), {
+      icon: starIcon
+    })
+    textMarker.addTo(theMap)
+    starMarker.addTo(theMap)
+  } catch (err) {
+    console.log("ERROR", err)
+  }
+}
+
+
+function drawGPXTrack(path, gpxJSON) {
   if (trackLayer) {
     theMap.removeLayer(trackLayer)
   }
@@ -373,20 +498,36 @@ function drawGPXTrack(path) {
     }
   }
   const customLayer = L.geoJson(null, {
-    style: trackStyle
+    style: trackStyle,
+
+    // filter out the points, because we'll draw those a different way
+    filter: thing => {
+      return thing.geometry.type.toLowerCase() != 'point'
+    }
   })
   trackLayer = omnivore.gpx(path, null, customLayer)
   trackLayer.addTo(theMap)
   trackLayer.on('ready', () => {
-    theMap.fitBounds(trackLayer.getBounds())
+    if (settings.has('mapZoom')) {
+      theMap.setZoom(parseInt(settings.get('mapZoom')))
+      theMap.invalidateSize()
+    } else {
+      theMap.fitBounds(trackLayer.getBounds())
+    }
   })
+
+  // draw names for the waypoints
+  gpxJSON.gpx.wpt.forEach(waypoint => {
+    addStarMarker(waypoint.name[0], waypoint.$.lat, waypoint.$.lon)
+  })
+
 }
 
 
 function extractTrackPoints(gpsData) {
   return (() => {
     try {
-      return gpsData.gpx.trk[0].trkseg[0].trkpt
+      return JSON.parse(JSON.stringify(gpsData.gpx.trk[0].trkseg[0].trkpt)) // clone it
     } catch (err) {
       return undefined
     }
@@ -411,30 +552,43 @@ function createMarker(lat, lon) {
 function handleGPXFile(path) {
   let gpxText = fs.readFileSync(path, 'utf-8')
   xml2jsParser.parseString(gpxText, (err, parsedGPX) => {
+    loadedGpxFile = path
     if (err) {
       // TODO show error dialog
       console.log('error parsing gpx file', err)
       return
     }
 
+    // const xml = builder.buildObject(parsedGPX)
+    // fs.writeFileSync('test-me.gpx', xml, 'utf-8')
+
     // TODO remove this , its only for debug
     global.gpsData = parsedGPX
+    gpxJSON = parsedGPX
     console.log(checkValidGPX(parsedGPX))
     trackPoints = extractTrackPoints(parsedGPX)
+
+    // map over trackPoints and compute and store a moment object for easy comparison later
+    trackPoints = trackPoints.map(p => {
+      p.moment = moment(p.time[0])
+      return p
+    })
     trackPositionElm.max = trackPoints.length
     const trackTime = moment(extractStartTime(parsedGPX))
     trackStartDatetimeElm.innerHTML = `Start: ${trackTime.format('ddd MMM DD h:mm:ss A')}`
     dateTimeElm.innerHTML = trackTime.format('dddd MMM DD, YYYY')
     titleTextInput.value = extractTrackName(parsedGPX)
-    titleElm.innerHTML = extractTrackName(parsedGPX)
+    titleElm.innerHTML = settings.get('title') || extractTrackName(parsedGPX) || ''
+    titleTextInput.value = settings.get('title') || extractTrackName(parsedGPX) || ''
     elevationElm.innerHTML = extractStartElevation(parsedGPX)
     const LL = extractStartLL(parsedGPX)
     trackMarker = createMarker(LL[0], LL[1])
     trackMarker.addTo(theMap)
-    latFieldElm.innerHTML = LL[0]
-    lonFieldElm.innerHTML = LL[1]
+    theMap.panTo(L.latLng(LL[0], LL[1]))
+    latFieldElm.innerHTML = parseFloat(LL[0]).toFixed(6)
+    lonFieldElm.innerHTML = parseFloat(LL[1]).toFixed(6)
 
-    drawGPXTrack(path)
+    drawGPXTrack(path, parsedGPX)
   })
 }
 
@@ -462,7 +616,6 @@ function handleTextOverlayDisplay() {
   showHideElm(dateTimeElm, showDateTimeElm.checked)
   showHideElm(speedElevationElm, showSpeedElevationElm.checked)
   showHideElm(latlonElm, showLLElm.checked)
-  showHideElm(headingElm, showHeadingElm.checked)
 }
 
 function handleTextColorInput() {
@@ -477,13 +630,7 @@ function handleTrackColorInput() {
   })
 }
 
-
-function handleTrackPositionChange() {
-  if (!trackPoints) {
-    return
-  }
-  trackIndex = this.value
-  console.log('track index', trackIndex, trackPoints[trackIndex])
+function updateDisplayFromTrackPosition() {
   const ll = L.latLng(trackPoints[trackIndex].$.lat, trackPoints[trackIndex].$.lon)
   theMap.panTo(ll)
   trackMarker.setLatLng(ll)
@@ -505,12 +652,167 @@ function handleTrackPositionChange() {
     unit: 'mph'
   })
   const heading = geolib.getBearing(prevPointTime, currentPointTime)
+  const compassDir = geolib.getCompassDirection(prevPointTime, currentPointTime)
   trackMarker.setRotationAngle(heading)
   speedFieldElm.innerHTML = Math.round(speedMPH)
   headingFieldElm.innerHTML = Math.round(heading)
-  latFieldElm.innerHTML = trackPoints[trackIndex].$.lat
-  lonFieldElm.innerHTML = trackPoints[trackIndex].$.lon
-  elevationElm.innerHTML = Math.round(trackPoints[trackIndex].ele[0] * feetPerMeter)  // TODO handle unit preference
+  compassElm.innerHTML = compassDir.exact
+  latFieldElm.innerHTML = parseFloat(trackPoints[trackIndex].$.lat).toFixed(6)
+  lonFieldElm.innerHTML = parseFloat(trackPoints[trackIndex].$.lon).toFixed(6)
+  elevationElm.innerHTML = Math.round(trackPoints[trackIndex].ele[0] * feetPerMeter) // TODO handle unit preference
+  dateTimeElm.innerHTML = moment(trackPoints[trackIndex].time[0]).format('ddd MMM DD, YYYY hh:mm:ss a')
+}
+
+
+function handleTrackPositionChange() {
+  if (!trackPoints) {
+    return
+  }
+
+  // set the global track index per the slider
+  trackIndex = this.value
+
+  if (locked) {
+    // this is the number of millis between the locked track position and the current track position
+    const millisecondsDiff = trackPoints[trackIndex].moment.diff(lockedPositions.moment)
+    console.log('msdiff', millisecondsDiff)
+
+    const framesDiff = (millisecondsDiff / 1000) / secondsPerVideoFrame
+    console.log('framesDiff', framesDiff)
+    const videoPosSec = lockedPositions.video + (millisecondsDiff / 1000)
+    console.log('videoPosSec', videoPosSec)
+    videoElm.currentTime = videoPosSec
+    videoPositionElm.value = (lockedPositions.video * framesPerSecond) + framesDiff
+  }
+
+  updateDisplayFromTrackPosition()
+
+  // if locked, move the video position in proportion
+  // if (locked) {
+  //   // figure out the proportional change
+  //   const delta = this.value - trackSliderPosition
+  //   const pct = delta / this.max
+  //   console.log('video position max', videoPositionElm.max)
+  //   console.log('delta, pct', delta, pct)
+  //   console.log('current video position', videoPositionElm.value)
+  //   console.log('changing video position slider by', videoPositionElm.max * pct)
+  //   videoPositionElm.value = parseInt(videoPositionElm.value) + Math.round(videoPositionElm.max * pct)
+  //   console.log('videoPositionElm.value now', videoPositionElm.value)
+  //   videoElm.currentTime = parseInt(videoPositionElm.value) / framesPerSecond
+  // }
+
+  trackSliderPosition = this.value
+}
+
+
+function handleChooseVideoType() {
+  switch (this.value || videoTypeElm.value) {
+    case 'regular-video':
+      secondsPerVideoFrame = 1 / framesPerSecond
+      break
+
+    case '1-sec-time-lapse':
+      secondsPerVideoFrame = 1
+      break
+
+    case '2-sec-time-lapse':
+      secondsPerVideoFrame = 2
+      break
+
+    case '3-sec-time-lapse':
+      secondsPerVideoFrame = 3
+      break
+
+    case '5-sec-time-lapse':
+      secondsPerVideoFrame = 5
+      break
+
+    case '30-sec-time-lapse':
+      secondsPerVideoFrame = 30
+      break
+
+    case '60-sec-time-lapse':
+      secondsPerVideoFrame = 60
+      break
+  }
+
+  if (this.value) {
+    settings.set('secondsPerVideoFrame', this.value)
+  }
+
+}
+
+function handleLock() {
+  if (locked) {
+    this.classList.remove('fa-lock')
+    this.classList.add('fa-unlock')
+  } else {
+    this.classList.add('fa-lock')
+    this.classList.remove('fa-unlock')
+  }
+
+  // if they are locking the video to the track, we need to bind the two together
+  // based on the current timestamp in the track
+  // so we'll make note of which position in the track equals which position
+  // in the video, and use that as a reference basis
+
+  locked = !locked
+  if (locked) {
+    lockedPositions.video = videoElm.currentTime,
+      lockedPositions.trackIndex = trackIndex,
+      lockedPositions.time = trackPoints[trackIndex].time[0],
+      lockedPositions.moment = moment(trackPoints[trackIndex].time[0])
+    console.log('lockedPositions', lockedPositions)
+  }
+}
+
+function handleTrackFocusBlur(event) {
+  console.log('track focus blur', this.id, event.type)
+  if (event.type == 'focus' && this.id === 'video-position') {
+    document.querySelector('.slider-w-label.video label').classList.add('selected')
+    document.querySelector('.slider-w-label.track label').classList.remove('selected')
+    activeSlider = 'video'
+  }
+
+  if (event.type == 'focus' && this.id === 'track-position') {
+    document.querySelector('.slider-w-label.track label').classList.add('selected')
+    document.querySelector('.slider-w-label.video label').classList.remove('selected')
+    activeSlider = 'track'
+  }
+
+  if (event.type === 'blur') {
+    document.querySelector('.slider-w-label.track label').classList.remove('selected')
+    document.querySelector('.slider-w-label.video label').classList.remove('selected')
+    activeSlider = undefined
+  }
+
+}
+
+function handleNewWaypoint(event) {
+  if (event.key === 'Enter') {
+    console.log('handle new waypoint', mapMouseEvent)
+    const newWaypoint = {
+      $: {
+        lat: `${mapMouseEvent.latlng.lat}`,
+        lon: `${mapMouseEvent.latlng.lng}`
+      },
+      name: this.value
+    }
+    gpxJSON.gpx.wpt.push(newWaypoint)
+    const xml = builder.buildObject(gpxJSON)
+
+    // TODO ok to write to the file we loaded?
+    fs.writeFileSync(loadedGpxFile, xml, 'utf-8')
+
+    // now add a marker
+    addStarMarker(this.value, mapMouseEvent.latlng.lat, mapMouseEvent.latlng.lng)
+
+    addWaypointDialog.classList.add('hidden')
+  }
+
+  if (event.key === 'Escape') {
+    addWaypointDialog.classList.add('hidden')
+  }
 }
 
 
@@ -518,10 +820,17 @@ showDateTimeElm.addEventListener('click', handleTextOverlayDisplay)
 showSpeedElevationElm.addEventListener('click', handleTextOverlayDisplay)
 showLLElm.addEventListener('click', handleTextOverlayDisplay)
 titleTextInput.addEventListener('input', handleTextOverlayDisplay)
-showHeadingElm.addEventListener('click', handleTextOverlayDisplay)
 
 titleTextInput.addEventListener('input', function() {
   titleElm.innerHTML = this.value
+  settings.set('title', this.value)
+})
+
+mapZoomElm.addEventListener('input', function() {
+  console.log('zooming to', this.value)
+  theMap.setZoom(this.value)
+  settings.set('mapZoom', this.value)
+  theMap.invalidateSize()
 })
 
 mapOpacityElm.addEventListener('input', function() {
@@ -547,21 +856,37 @@ tileLayerElm.addEventListener('change', handleChooseTileLayer)
 
 playPauseElm.addEventListener('click', handlePlayPause)
 
+videoTypeElm.addEventListener('change', handleChooseVideoType)
+
+videoPositionElm.addEventListener('focus', handleTrackFocusBlur)
+trackPositionElm.addEventListener('focus', handleTrackFocusBlur)
+videoPositionElm.addEventListener('blur', handleTrackFocusBlur)
+trackPositionElm.addEventListener('blur', handleTrackFocusBlur)
 
 playbackSpeedElm.addEventListener('input', function() {
   videoElm.playbackRate = this.value / 100
+  settings.set('playbackSpeed', this.value)
 })
+
+trackVideoLockElm.addEventListener('click', handleLock)
 
 trackPositionElm.addEventListener('input', handleTrackPositionChange)
 
 videoPositionElm.addEventListener('input', function() {
   videoElm.currentTime = this.value / framesPerSecond
+  // compute the real world time of this video position and find the closest track point
+  if (locked) {
+    trackIndex = getTrackIndexForVideoPosition(true) // allowBackwards is true
+    trackPositionElm.value = trackIndex
+    updateDisplayFromTrackPosition()
+  }
+
 })
 
 videoElm.addEventListener('play', () => {
   playPauseElm.classList.remove('fa-play')
   playPauseElm.classList.add('fa-pause')
-  checkVideoInterval = setInterval(checkVideo, 500)
+  checkVideoInterval = setInterval(checkVideo, 100)
 })
 
 videoElm.addEventListener('pause', () => {
@@ -570,5 +895,28 @@ videoElm.addEventListener('pause', () => {
   clearInterval(checkVideoInterval)
 })
 
+newWaypointElm.addEventListener('keydown', handleNewWaypoint)
+
 initializeMap()
 loadSettings()
+
+
+/**
+  • add icon names to map
+  The simplest way to handle this will be to just parse the gpx ourselves (we already do this)
+  and add our own markers to the map.
+
+  The current approach involves adding a hook that gets called for each 'feature' as it's added
+  that's more for attaching popups and stuff.  Let's just add more markers... simpler
+
+  • fix independent sliders
+    It might be usful to have one method that does the video / track syncing which is used
+    both during play AND scrubbing
+
+  • play button behavior when unlocked?
+    When unlocked, what should the play button do?
+
+  • add points
+    I want the ability to add points with labels in this tool.  This will be useful because as I
+    watch the video with the track, I want to add things like 'this is a good lunch spot'
+*/
